37e9109384f0f9cab2751fe031750601f181c61e
Deliquescence <misterbeez01@gmail.com>
2014-05-25 00:29:12 -0400

    From http://netty.io/wiki/user-guide-for-4.x.html#wiki-h3-6

--- src/Deliquescence/Network/Server.java
+++ src/Deliquescence/Network/Server.java
@@ -6,8 +6,15 @@
 package Deliquescence.Network;
 
 import Deliquescence.Config;
-import java.net.ServerSocket;
-import java.net.Socket;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.example.discard.DiscardServerHandler;
 
 /**
  *
@@ -15,20 +22,34 @@ import java.net.Socket;
  */
 public class Server {
 
-    public void test() {
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
+    private String[] playerNames;
 
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
-            serverSocket = new ServerSocket(Config.getInt("LAN_PORT"));
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128) // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
 
-        try {
-            serverSocket.accept();
-        } catch (Exception e) {
+            // Bind and start to accept incoming connections.
+            ChannelFuture f = b.bind(Config.getInt("NETWORK_PORT")).sync(); // (7)
 
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
         }
     }
 }
