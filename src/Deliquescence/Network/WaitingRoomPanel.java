9bfb1c44c823acfd15c48d14741ae334d8d8be91
Deliquescence <misterbeez01@gmail.com>
2015-10-06 16:47:25 -0400

    Remove names button

--- src/Deliquescence/Network/WaitingRoomPanel.java
+++ src/Deliquescence/Network/WaitingRoomPanel.java
@@ -146,7 +146,6 @@ public class WaitingRoomPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         StartButton = new javax.swing.JButton();
-        RequestNamesButton = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         NamesPanel = new javax.swing.JPanel();
@@ -165,14 +164,6 @@ public class WaitingRoomPanel extends javax.swing.JPanel {
             }
         });
         add(StartButton);
-
-        RequestNamesButton.setText("names");
-        RequestNamesButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                RequestNamesButtonActionPerformed(evt);
-            }
-        });
-        add(RequestNamesButton);
 
         jButton1.setText("Edit Settings (Host Only) (TODO)");
         add(jButton1);
@@ -220,14 +211,6 @@ public class WaitingRoomPanel extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_StartButtonActionPerformedWaitingRoom
 
-    private void RequestNamesButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RequestNamesButtonActionPerformed
-        if (isServer) {
-            server.getNames();
-        } else {
-            client.sendDebugPacket();
-        }
-    }//GEN-LAST:event_RequestNamesButtonActionPerformed
-
     public void startGame(GameClient client) {
         Log.trace("wrp.startgame");
         //NetworkGameViewer ngv = (NetworkGameViewer) this.getParent();
@@ -265,7 +248,6 @@ public class WaitingRoomPanel extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton LeaveButton;
     private javax.swing.JPanel NamesPanel;
-    private javax.swing.JButton RequestNamesButton;
     private javax.swing.JButton StartButton;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
