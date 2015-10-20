8728ac5c60da8769aef746a2c6e75e3e885e672c
Deliquescence <misterbeez01@gmail.com>
2015-10-20 16:34:19 -0400

    Remove junk

--- src/Deliquescence/Network/NetworkSetupPanel.java
+++ src/Deliquescence/Network/NetworkSetupPanel.java
@@ -63,10 +63,6 @@ public class NetworkSetupPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
         jSplitPane1 = new javax.swing.JSplitPane();
         LeftPanel = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
@@ -76,34 +72,6 @@ public class NetworkSetupPanel extends javax.swing.JPanel {
         StartButton = new javax.swing.JButton();
         RightPanel = new javax.swing.JPanel();
         gameSettings1 = new Deliquescence.Panel.GameSettings();
-
-        jButton1.setText("make server");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
-        jButton2.setText("make client");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-
-        jButton3.setText("client send");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
-
-        jButton4.setText("server send");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
-            }
-        });
 
         setLayout(new javax.swing.BoxLayout(this, javax.swing.BoxLayout.LINE_AXIS));
 
@@ -143,36 +111,6 @@ public class NetworkSetupPanel extends javax.swing.JPanel {
         add(jSplitPane1);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        //createServer();
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        //createClient();
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        /*try {
-         MyNetworkObject mno = new MyNetworkObject();
-         mno.text = "Client send";
-         client.sendTCP(mno);
-         //System.out.println("attempted client send");
-         } catch (Exception e) {
-         System.err.println(e);
-         }*/
-    }//GEN-LAST:event_jButton3ActionPerformed
-
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        /*try {
-         MyNetworkObject mno = new MyNetworkObject();
-         mno.text = "Server send";
-         server.sendToAllTCP(mno);
-         //System.out.println("attempted server send");
-         } catch (Exception e) {
-         System.err.println(e);
-         }*/
-    }//GEN-LAST:event_jButton4ActionPerformed
-
     private void StartButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_StartButtonActionPerformed
         GameManager man = (GameManager) this.getParent().getParent();//Sketchy
         man.switchToTabByTitle("Games");
@@ -197,35 +135,6 @@ public class NetworkSetupPanel extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_StartButtonActionPerformed
 
-    /* private void createServer() {
-     try {
-     server = new Server();
-     server.addListener(new ServerListener());
-     server.start();
-
-     server.bind(Config.getInt("NETWORK_PORT"));
-
-     /*Kryo kryo = server.getKryo();
-     kryo.register(MyNetworkObject.class);
-     } catch (Exception e) {
-     System.err.println(e);
-     }
-     }
-
-     private void createClient() {
-     try {
-     client = new Client();
-     client.addListener(new ClientListener());
-     client.start();
-
-     client.connect(5000, "localhost", Config.getInt("NETWORK_PORT"));
-
-     /*Kryo kryo = client.getKryo();
-     kryo.register(MyNetworkObject.class);
-     } catch (Exception e) {
-     System.err.println(e);
-     }
-     }*/
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel LeftPanel;
     private javax.swing.JSlider LocalPlayersSlider;
@@ -233,10 +142,6 @@ public class NetworkSetupPanel extends javax.swing.JPanel {
     private javax.swing.JButton StartButton;
     private Deliquescence.Panel.GameSettings gameSettings1;
     private Deliquescence.Panel.GameSliders gameSliders1;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSplitPane jSplitPane1;
